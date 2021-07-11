372f30023ff18dc2c4ff73d6efbe31903ed79264
David Schnitzhofer <david@schnitzhofer.at>
2021-07-11 11:46:45 +0200

    Added content and styling for list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularHosted';
+
+  public items = ['This', 'is', 'a', 'mother', 'flipping', 'test.', 'I', 'came', 'here', 'to', 'drink', 'milk', 'and', 'kick', 'ass', 'and', 'I', 'just', 'finished', 'my', 'milk!',
+    'This', 'is', 'a', 'mother', 'flipping', 'test.', 'I', 'came', 'here', 'to', 'drink', 'milk', 'and', 'kick', 'ass', 'and', 'I', 'just', 'finished', 'my', 'milk!'];
 }
